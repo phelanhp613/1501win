c22c1d9139ffad259c665e08318a4bceebd074dc
150win-2 <150win2@protonmail.com>
2023-03-04 16:29:44 +0700

    Index 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,36 @@
 (function ($) {
-    "use strict";
+	"use strict";
 
-    let fn = {
-        SlickSlider: function (slider) {
-            slider.slick({
-                autoplay: true,
-                autoplaySpeed: 2000,
-            });
-        },
-        SlickSyncing: function (tabFor, tabNav, slidesToShow, slidesToScroll) {
-            $(tabFor).slick({
-	            slidesToShow: 1,
-	            slidesToScroll: 1,
-	            arrows: true,
-	            fade: false,
-	            infinite: false,
-	            useTransform: true,
-	            speed: 400,
-	            adaptiveHeight: true,
-	            asNavFor: tabNav,
-            });
-            $(tabNav).slick({
-                slidesToShow: slidesToShow,
-                slidesToScroll: slidesToScroll,
-                dots: false,
-                focusOnSelect: true,
-                infinite: false,
-	            asNavFor: tabFor,
-            });
-            $(tabNav).on('click', '.slick-slide', function (event) {
-                event.preventDefault();
-                var goToSingleSlide = $(this).data('slick-index');
-	            $(tabFor).slick('slickGoTo', goToSingleSlide);
-            });
-        }
-    }
+	let fn = {
+		SlickSlider: function (slider) {
+			slider.slick({
+				autoplay: true,
+				autoplaySpeed: 2000,
+			});
+		},
+		SlickSyncing: function (tabFor, tabNav, slidesToShow, slidesToScroll) {
+			$(tabFor).slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: false,
+			});
+			$(tabNav).slick({
+				slidesToShow: slidesToShow,
+				slidesToScroll: slidesToScroll,
+				infinite: false,
+			});
+			$(tabNav).on('click', '.slick-slide', function (event) {
+				event.preventDefault();
+				$(".slick-slide").removeClass('active');
+				$(this).addClass('active');
+				$(tabFor).slick('slickGoTo', $(this).data('slick-index'));
+			});
+		}
+	}
 
 	$(document).ready(function () {
-        fn.SlickSlider($('.home-banner-slider'));
-        fn.SlickSyncing('.tab-for', '.tab-nav', 5, 5);
+		fn.SlickSlider($('.home-banner-slider'));
+		fn.SlickSyncing('.tab-for', '.tab-nav', 5, 5);
 		fn.SlickSyncing('.transfer-tab-for', '.transfer-tab-nav', 3, 3);
-    });
+	});
 })(jQuery);
